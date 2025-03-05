1df68d96ad85e93110b59f7cdd5601edb2296bd6
danng <danhoat@gmail.com>
2025-03-05 11:36:57 +0700

    update grammar

--- template/fullstack.php
+++ template/fullstack.php
@@ -112,7 +112,7 @@
 				<div class="item">
 					<label class="sub-label">Freelance Full Stack Developer</label>
 					<p> Jun 2024 - Present</p>
-					<p>I'm working as a full stack web developer. Custom function for WordPress website and Laravel(VueJs) Framework</p>
+					<p>I'm working as a full stack web developer. Custom function for WordPress website and Laravel(VueJs) framework.</p>
 
 				</div>
 
