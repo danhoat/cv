9904312bb624e46bef5de26f053c3dde83faa5c0
danng <danhoat@gmail.com>
2025-12-04 21:13:10 +0700

    add cover

---
+++ cover-letter.php
@@ -0,0 +1,83 @@
+
+<?php
+global $is_index;
+$is_index = 1;
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once('define.php');
+require_once('functions.php');
+?>
+<?php get_header(); ?>
+
+<div class="container 1-column cover-letter">
+	
+		<div class="row wrap-cover top">
+            <div class="" style="padding: 30px 0;">
+			<!-- <div class="col-3 col-avatar">
+				<img class="avatar" src="<?php echo IMG_URL;?>/danng.png">
+			</div> -->
+			<div class="col-12 ">
+				<h1 class="fullname"> Dan Nguyen Gia</h1>
+				<h3 class="job-title"> WIX WEBSITE DEVELOPER  </h3>
+				<p> +84 916681139 danhoat@gmail.com</p>
+				<p> 523 Do Xuan Hop, P. Phuoc Long B, Ho Chi Minh City</p>
+			</div>
+		</div>
+	</div>
+	<div class ="line-split"><span></span><span></span><span></span></div>
+   <div class="row">
+
+		<div class="col-12 col-right col-content">
+
+            <div class="line skill-line" style="line-height: 29px; font-size: 17px;;">
+                <p> 04 December 2025</p>
+                <hr style=""/>
+                <tr
+                <p>
+                    Hiring Manager <br />
+                    MASX ENTERTAINMENT INC. <br />
+                
+
+
+                </p>
+            </div>
+        </div>
+    </div>
+	<div class="row main" style="margin-top:40px !important;">
+
+		<div class="col-12 col-right col-content">
+            <h3 class="label" style="margin-bottom: 30px;"> Cover Letter </h3>   
+	
+
+			
+			<div class="line skill-line" style="line-height: 29px; font-size: 17px;;">
+				<p> I am excited to apply for the Web Developer position.<p> 
+                    <p> 
+
+                With over ten years of experience building high-performance web applications, I specialize in system architecture, complex integrations, and scalable code. I take pride in writing clean, meticulous, and elegant solutions that stand the test of time.
+
+                </p> <p> 
+
+                Beyond my technical passion, I have always been drawn to Canada’s natural beauty. Contributing to your team would be both a professional privilege and may have a chance to visit Canada in life.
+                </p> 
+
+                I am ready to make an immediate, long-term commitment to your team. I firmly believe that choosing me would be an valuable investment for your company, and I look forward to the opportunity to discuss how I can contribute to your success."
+
+                Thank you for your consideration.
+                 </p>
+                 <p> <br /> <br /> </p>
+
+                Sincerely,<br />
+                <p style="font-weight: 430;"> Dan Nguyen </p>
+                </p>
+                <p> </p><br />
+              
+			</div>
+		
+	</div>
+</div>
+
+
+
+<?php get_footer();?>
+
